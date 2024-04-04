9ebc51af7b14ae59b6deec1b90805af44b39f3d7
Luca Andrey <lucaandrey5@hotmail.com>
2024-04-04 08:40:28 -0300

    initial project

---
+++ src/controllers/controller.go
@@ -0,0 +1,71 @@
+package controllers
+
+import (
+	"gin-todoapp/src/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func GetTodos(c *gin.Context) {
+	var todo []models.Todo
+
+	err := models.GetAllTodos(&todo)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, todo)
+	}
+}
+
+func CreateATodo(c *gin.Context) {
+	var todo models.Todo
+	c.BindJSON(&todo)
+	err := models.CreateATodo(&todo)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+	} else {
+		c.JSON(http.StatusCreated, todo)
+	}
+}
+
+func GetATodo(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var todo models.Todo
+
+	err := models.GetATodo(&todo, id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, todo)
+	}
+
+}
+
+func UpdateATodo(c *gin.Context) {
+	var todo models.Todo
+	id := c.Params.ByName("id")
+	err := models.GetATodo(&todo, id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, todo)
+	}
+
+	c.BindJSON(&todo)
+	err = models.UpdateATodo(&todo, id)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, todo)
+	} else {
+		c.JSON(http.StatusOK, todo)
+	}
+}
+
+func DeleteATodo(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var todo models.Todo
+	err := models.DeleteATodo(&todo, id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, todo)
+	} else {
+		c.JSON(http.StatusOK, gin.H{"id: " + id: "deleted"})
+	}
+}
---
+++ src/models/database.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"fmt"
+	"gin-todoapp/src/config"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// fetch all todos at once
+func GetAllTodos(todo *[]Todo) (err error) {
+	if err = config.DB.Find(todo).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func CreateATodo(todo *Todo) (err error) {
+	if err := config.DB.Create(todo).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetATodo(todo *Todo, id string) (err error) {
+	if err := config.DB.Where("id = ?", id).First(todo).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func UpdateATodo(todo *Todo, id string) (err error) {
+	fmt.Println(todo)
+	config.DB.Save(todo)
+	return nil
+}
+
+func DeleteATodo(todo *Todo, id string) (err error) {
+	config.DB.Where("id = ?", id).Delete(todo)
+	return nil
+}
